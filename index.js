8b191b2bbf1316ab6347905dd0616db75bb51bb2
Thiago-PC <thiagofoz@gmail.com>
2020-03-26 10:10:45 -0300

    tratando arquivos

--- index.js
+++ index.js
@@ -1,32 +1,42 @@
-const express = require('express');
+const express = require("express");
 
-const formidableMiddleware = require('express-formidable');
-const formidable = require('formidable');
-const path = require('path');
+const formidableMiddleware = require("express-formidable");
+const formidable = require("formidable");
+const path = require("path");
 
 const app = express();
 
-app.use(formidableMiddleware({
-    encoding: 'utf-8',
-    uploadDir: path.join(__dirname + '/files-temporary'),
-    multiples: true, // req.files to be arrays of files
-}, {
-    event: 'end',
-    action: function (req, res, next, name, file) {
-        console.log(name);
+const form = formidable({
+    multiples: true,
+    uploadDir: __dirname + "/files-temporary/",
+    keepExtensions: true
+  });
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/view/index.html");
+});
+
+app.get("/upload-files", (req, res) => {
+    res.sendFile(__dirname + "/view/upload-files.html")
+})
+
+form.on("file", (filename, file) => {
+  form.emit("data", { name: "file", key: filename, value: file });
+});
+
+app.post("/upload-files", (req, res) => {
+  
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
     }
-));
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/view/index.html')
-})
-app.post('/upload-files', (req, res) => {
-    
-    res.sendFile(__dirname + '/view/index.html')
-
-})
+    //console.log(fields);
+   // console.log(files);
+  });
+  res.redirect("/view/upload-files.html");
+});
 
 app.listen(3000, () => {
-    console.log('Listening on port 3k');
-
-})
+  console.log("Listening on port 3k");
+});
